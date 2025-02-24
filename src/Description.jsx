5cd4c6e1feb8327e7cfb72eaf2ce90ad1630457f
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-24 18:59:38 -0300

    Iniciando descricao

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,13 +1,16 @@
-import { HStack, Avatar } from "@chakra-ui/react";
+import { HStack, Avatar, Flex, Text } from "@chakra-ui/react";
 import profile from "./assets/profile.jpeg";
 
 function App() {
     return (
         <>
         <HStack>
-            <Avatar.Root size={"2xl"} variant={"outline"}>
-                <Avatar.Image src={profile}></Avatar.Image>
-            </Avatar.Root>
+            <Flex>
+                <Text textStyle="xl" fontWeight="medium">Sobre Mim</Text>
+                <Avatar.Root size="2xl" variant="outline">
+                    <Avatar.Image src={profile}></Avatar.Image>
+                </Avatar.Root>
+            </Flex>
         </HStack>
         </>
     )
