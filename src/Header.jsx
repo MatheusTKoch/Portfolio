d90e1be2d1d6ca0ef8c85fb9d9a2d49334e991f5
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-24 14:24:52 -0300

    Adicionando icones das redes no header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,23 @@
 import { FaGithub } from "react-icons/fa";
-import { Icon, HStack } from "@chakra-ui/react";
+import { FaLinkedin } from "react-icons/fa";
+import { IconButton, HStack, Link } from "@chakra-ui/react";
+import './Header.css';
+
 function App() {
     return (
         <>
             <div className="conteudo">
                 <HStack>
-                    <Icon>
-                        <FaGithub></FaGithub>
-                    </Icon>
+                    <Link href="https://github.com/MatheusTKoch" target="_blank">
+                        <IconButton size="xl" variant="ghost" rounded="full">
+                            <FaGithub size="xl"></FaGithub>
+                        </IconButton>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/matheus-trilha-koch-712806162/" target="_blank">
+                        <IconButton size="xl" variant="ghost" rounded="full">
+                            <FaLinkedin size="xl"></FaLinkedin>
+                        </IconButton>
+                    </Link>
                 </HStack>
             </div>
         </>
