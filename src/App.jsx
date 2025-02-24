b5129e81f651d3505b8c66d865bb1794958dacdf
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-23 22:31:13 -0300

    Ajustando icone

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Avatar, HStack } from "@chakra-ui/react";
+
 import profile from "../src/assets/profile.jpeg"
 import './App.css'
 
 function App() {
-let size = "md";
 
   return (
     <>
-      <div>
+      <div className="conteudo"> 
         <HStack>
           <h1>Olá, eu sou o Matheus </h1>
-          <Avatar.Root size={size}>
+          <Avatar.Root size="2xl" variant="outline">
             <Avatar.Image src={profile}></Avatar.Image>
           </Avatar.Root>
         </HStack>
