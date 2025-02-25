05fd559c7c1e324274ad40a1fb14ec2e77970224
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-25 10:56:22 -0300

    Adicionando tecnologias

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,27 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Text, IconButton, Box} from "@chakra-ui/react";
+import { FaVuejs, FaReact, FaNodeJs } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
 
 function App() {
     return (
         <>
-        <HStack>
-            <Text textStyle="xl" fontWeight="medium" position="relative" left="16vw" top="10vh">Tecnologias</Text>
-
-        </HStack>
+        <Box position="relative" top="10vh">
+            <Text textStyle="xl" fontWeight="medium" position="relative" right="2vw">Tecnologias</Text>
+            <HStack position="relative" left="12vw" paddingTop="1vh">
+                <IconButton size="xl" variant="ghost" rounded="xl" colorPalette="green">
+                    <FaVuejs></FaVuejs>
+                </IconButton>
+                <IconButton size="xl" variant="ghost" rounded="xl" colorPalette="blue">
+                    <FaReact></FaReact>
+                </IconButton>
+                <IconButton size="xl" variant="ghost" rounded="xl" colorPalette="green">
+                    <FaNodeJs></FaNodeJs>
+                </IconButton>
+                <IconButton size="xl" variant="ghost" rounded="xl" colorPalette="yellow">
+                    <IoLogoJavascript></IoLogoJavascript>
+                </IconButton> 
+            </HStack>
+        </Box>
         </>
     )
 }
