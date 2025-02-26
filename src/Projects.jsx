78ebebfa8c649385e34c50b1356395537dc6611a
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-25 22:31:21 -0300

    Ajustes nos cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,16 +5,15 @@ function App() {
         <>
         <Text textStyle="xl" fontWeight="medium" position="relative" top="15vh">Projetos</Text>
             <Center position="relative" top="10vh" paddingTop="10vh">
-                
                 <Card.Root position="relative" height="40vh" width="30vw" padding="2%">
                     <Card.Body>
                         <Image></Image>
                     </Card.Body>
-                    <Card.Title>Weather App</Card.Title>
-                    <Card.Description>
+                    <Card.Title textAlign="center">Weather App</Card.Title>
+                    <Card.Description padding="5%">
                         Aplicativo para detalhamento da previsão atual.
                     </Card.Description>
-                    <Button position="relative" left="10vw" width="10vw" >Visitar</Button>
+                    <Button variant="outline" textAlign="center" color="whitesmoke" >Visitar</Button>
                 </Card.Root>
             </Center>
         </>
