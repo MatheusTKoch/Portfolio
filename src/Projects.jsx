e09f3d36ab3d78fc5e4d3fcd631eff24d9a4c7b8
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-26 17:07:34 -0300

    Adicionando imagens aos projetos

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,7 +8,7 @@ const projectsData = [
     id: 1,
     title: "Weather App",
     description: "Aplicativo para detalhamento da previsão atual com suporte para múltiplas cidades e visualização de dados.",
-    image: "/images/weather-app.jpg",
+    image: "/src/assets/weather_app.png",
     technologies: ["VueJS", "Flask", "Open Weather API"],
     demoUrl: "https://matheustkoch.github.io/weather_app/",
     repoUrl: "https://github.com/MatheusTKoch/weather_app"
@@ -17,7 +17,7 @@ const projectsData = [
     id: 2,
     title: "Divismart",
     description: "Plataforma para controle e acompanhamento de investimentos, com ativos como ações, fundos imobiliários e tesouro direto. Também conta com gráficos para auxiliar na visualização - em progresso.",
-    image: "/images/task-manager.jpg", 
+    image: "/src/assets/divismart.png",
     technologies: ["Vue", "ExpressJS", "MySQL", "Cheerio"],
     demoUrl: "",
     repoUrl: "https://github.com/MatheusTKoch/DiviSmart"
@@ -26,7 +26,7 @@ const projectsData = [
     id: 3,
     title: "Task Project",
     description: "Aplicativo para controle execução de tarefas diárias.",
-    image: "/images/ecommerce.jpg", 
+    image: "/src/assets/task_project.png", 
     technologies: ["Vue", "Firebase", "ExpressJS"],
     demoUrl: "https://matheustkoch.github.io/task-project/",
     repoUrl: "https://github.com/MatheusTKoch/task-project"
@@ -54,7 +54,7 @@ const ProjectCard = ({ project }) => {
       height="100%"
     >
       <Image
-        src={project.image || "/api/placeholder/400/200"}
+        src={project.image}
         alt={project.title}
         objectFit="cover"
         height="200px"
@@ -128,7 +128,7 @@ ProjectCard.propTypes = {
 
 function App() {
   return (
-    <Container maxW="container.xl" py={20}>
+    <Container maxW="container.xl" top="10vh" py={20}>
       <VStack spacing={12}>
         <Heading
           fontSize="3xl"
