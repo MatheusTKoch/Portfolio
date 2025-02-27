093db709bbd162096af6383b2449ebaa28522d66
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-27 13:01:38 -0300

    Adicionando ajustes no titulo

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,15 +1,51 @@
-import { HStack, Center, Text } from "@chakra-ui/react";
+import { Box, Text, Flex, useBreakpointValue } from "@chakra-ui/react";
 
-function App() {
-    return (
-        <>
-            <Center>
-                <HStack position="relative" top="10vh" paddingBottom="5%">
-                    <Text color="whitesmoke" fontSize="xxx-large">Olá, eu sou o Matheus <span>👋</span></Text>
-                </HStack>
-            </Center>
-        </>
-    )
-};
+function Title() {
+  const fontSize = useBreakpointValue({
+    base: "xl",
+    sm: "2xl",
+    md: "3xl",
+    lg: "4xl"
+  });
+  
+  const spacing = useBreakpointValue({
+    base: 2,
+    md: 4
+  });
 
-export default App;
+  const iconSize = useBreakpointValue({
+    base: "24px",
+    md: "32px",
+    lg: "40px"
+  });
+
+  return (
+    <Box 
+      as="section"
+      py={12}
+      width="100%"
+    >
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        align={{ base: "center", md: "center" }}
+        justify="center"
+        maxW="1200px"
+        mx="auto"
+        gap={spacing}
+      >
+        <Text
+          fontSize={fontSize}
+          fontWeight="bold"
+          textAlign={{ base: "center", md: "left" }}
+        >
+          Olá, eu sou o Matheus
+        </Text>
+        <Text fontSize={iconSize} display="inline-block">
+          👋
+        </Text>
+      </Flex>
+    </Box>
+  );
+}
+
+export default Title;
