1912226df01d6265e9633e18b7885a6631c7be81
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-28 11:00:51 -0300

    Ajustado scroll e cores do background

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,49 @@ import Description from "./Description.jsx";
 import Tecnologies from "./Tecnologies.jsx";
 import Projects from "./Projects.jsx";
 import Timeline from "./Timeline.jsx";
+import { Box } from "@chakra-ui/react";
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      <Header></Header>
-      <Title></Title>
-      <Description></Description>
-      <Tecnologies></Tecnologies>
-      <Projects></Projects>
-      <Timeline></Timeline>
-    </>
-  )
+    <Box
+      position="relative"
+      minH="100vh"
+      _before={{
+        content: '""',
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        bgGradient: "linear(to-br, gray.900, blue.900)",
+        opacity: 0.97,
+        zIndex: -2
+      }}
+      _after={{
+        content: '""',
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundImage: 
+          "radial-gradient(circle at 25% 25%, rgba(255,255,255,0.2) 1px, transparent 1px), " +
+          "radial-gradient(circle at 75% 75%, rgba(255,255,255,0.2) 1px, transparent 1px)",
+        backgroundSize: "50px 50px",
+        opacity: 0.4,
+        zIndex: -1,
+        animation: "backgroundMove 60s linear infinite"
+      }}
+    >
+      <Header />
+      <Title />
+      <Description />
+      <Tecnologies />
+      <Projects />
+      <Timeline />
+    </Box>
+  );
 }
 
-export default App
+export default App;
