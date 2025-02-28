3e27d373c2c77c1799ddfe853447c97df07edbe1
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-27 21:20:55 -0300

    Melhorias na timeline e responsividade adicionada

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,57 +1,68 @@
-import { Center, Text } from "@chakra-ui/react";
+import { Box, Text, VStack, Container } from "@chakra-ui/react";
 import { TimelineConnector, TimelineContent, TimelineDescription, TimelineItem, TimelineRoot, TimelineTitle } from "@/components/ui/timeline";
-import { FaCashRegister,FaProjectDiagram } from "react-icons/fa";
+import { FaCashRegister, FaProjectDiagram } from "react-icons/fa";
 import { BiSupport } from "react-icons/bi";
 
-function App() {
+function Timeline() {
     return (
-        <>
-            <Text position="relative" top="20vh" fontSize="xl" fontWeight="medium">Trajetória Profissional</Text>
-            <Center position="relative" top="25vh">
-                
-                <TimelineRoot maxW="400px">
-                    <TimelineItem>
-                        <TimelineConnector>
-                            <BiSupport></BiSupport>
-                        </TimelineConnector>
-                        <TimelineContent>
-                        <TimelineTitle>Suporte de Sistema ERP</TimelineTitle>
-                        <TimelineDescription>jul de 2022 - o momento</TimelineDescription>
-                        <Text textStyle="md" textAlign="justify">
-                            Atendimento, treinamento e suporte a cliente de sistema ERP;
-                            Instalação e suporte de equipamentos como impressoras e demais equipamentos fiscais;
-                            Manutenção e correção de erros relacionados a bancos de dados SQL;
-                        </Text>
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineConnector> 
-                            <FaProjectDiagram></FaProjectDiagram>
-                        </TimelineConnector>
-                        <TimelineContent>
-                        <TimelineTitle>Estagiário de Engenharia</TimelineTitle>
-                        <TimelineDescription>fev de 2021 - dez de 2021</TimelineDescription>
-                        <Text textStyle="md" textAlign="justify" >
-                            Estagiário na área de engenharia de produtos.
-                        </Text>
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineConnector>
-                            <FaCashRegister></FaCashRegister>
-                        </TimelineConnector>
-                        <TimelineContent>
-                        <TimelineTitle>Atendente</TimelineTitle>
-                        <TimelineDescription>out de 2017 - jan de 2021</TimelineDescription>
-                        <Text textStyle="md" textAlign="justify">
-                            Atendente na área de alimentos, atuando como repositor e manutenção do software da empresa.
-                        </Text>
-                        </TimelineContent>
-                    </TimelineItem>
-                </TimelineRoot>
-            </Center>
-        </>
-    )
-};
+        <Box
+            as="section"
+            py={{ base: 8, md: 16 }}
+        >
+            <Container maxW="container.lg">
+                <Text
+                    fontSize={{ base: "2xl", md: "3xl" }}
+                    fontWeight="bold"
+                    textAlign="center"
+                    mb={{ base: 6, md: 10 }}
+                >
+                    Trajetória Profissional
+                </Text>
+                <VStack spacing={4} align="stretch">
+                    <TimelineRoot>
+                        <TimelineItem>
+                            <TimelineConnector>
+                                <BiSupport></BiSupport>
+                            </TimelineConnector>
+                            <TimelineContent>
+                                <TimelineTitle>Suporte de Sistema ERP</TimelineTitle>
+                                <TimelineDescription>jul de 2022 - o momento</TimelineDescription>
+                                <Text textStyle="md" textAlign="justify">
+                                    Atendimento, treinamento e suporte a cliente de sistema ERP;
+                                    Instalação e suporte de equipamentos como impressoras e demais equipamentos fiscais;
+                                    Manutenção e correção de erros relacionados a bancos de dados SQL;
+                                </Text>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineConnector>
+                                <FaProjectDiagram></FaProjectDiagram>
+                            </TimelineConnector>
+                            <TimelineContent>
+                                <TimelineTitle>Estagiário de Engenharia</TimelineTitle>
+                                <TimelineDescription>fev de 2021 - dez de 2021</TimelineDescription>
+                                <Text textStyle="md" textAlign="justify">
+                                    Estagiário na área de engenharia de produtos.
+                                </Text>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineConnector>
+                                <FaCashRegister></FaCashRegister>
+                            </TimelineConnector>
+                            <TimelineContent>
+                                <TimelineTitle>Atendente</TimelineTitle>
+                                <TimelineDescription>out de 2017 - jan de 2021</TimelineDescription>
+                                <Text textStyle="md" textAlign="justify">
+                                    Atendente na área de alimentos, atuando como repositor e manutenção do software da empresa.
+                                </Text>
+                            </TimelineContent>
+                        </TimelineItem>
+                    </TimelineRoot>
+                </VStack>
+            </Container>
+        </Box>
+    );
+}
 
-export default App;
+export default Timeline;
