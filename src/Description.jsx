a1b24bdfae8160d4b0989308ab2e1ae28208fff1
Matheus Trilha Koch <matheust_koch@hotmail.com>
2025-02-28 22:45:30 -0300

    Realizando ajustes na localizacao

--- src/Description.jsx
+++ src/Description.jsx
@@ -15,7 +15,7 @@ function Description() {
                 >
                     <Avatar
                         size={{ base: "xl", md: "2xl" }}
-                        src={"assets/profile.jpeg"}
+                        src={"/assets/profile.jpeg"}
                         border="2px solid"
                         borderColor="gray.400"
                         _hover={{
